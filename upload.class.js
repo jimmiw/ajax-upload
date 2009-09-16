0129be031e8a2f364244e56394a84ffa8306e637
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2009-09-17 00:09:22 +0200

    Did some testing. Changed the version to 0.3

--- upload.class.js
+++ upload.class.js
@@ -1,7 +1,7 @@
 var Westsworld = (Westsworld == null)? {} : Westsworld;
 
 Westsworld.Upload = Class.create({
-	version: '1.0',
+	version: '0.3',
 	
 	initialize: function(options) {
 		this.setOptions(options);
